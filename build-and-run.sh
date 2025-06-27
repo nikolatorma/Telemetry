520ec474d839fc7c96e7b6ef64abaae4e37c6b8b
Nikola Torma <nidzatorma97@live.com>
2025-06-27 16:43:32 +0200

    Some workflow and docker changes

--- build-and-run.sh
+++ build-and-run.sh
@@ -6,7 +6,7 @@ echo "Building Spring Boot app with Maven..."
 
 # Run Docker Compose (build + start services)
 echo "Starting MySQL and Spring Boot app via Docker Compose..."
-docker compose -f docker-compose.yml up --build -d
+docker compose -f docker-compose.yaml up --build -d
 
 # Run tests after containers are up
 echo "Running integration and unit tests..."
